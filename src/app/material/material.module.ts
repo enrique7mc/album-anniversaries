9094e944e9b7aa0fadf46e938b9eaf5d476654fe
Enrique Munguía <enrique7mc@gmail.com>
2019-01-21 13:39:55 -0800

    Add progress bar when loading

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,6 +5,7 @@ import {
   MatIconModule,
   MatListModule,
   MatMenuModule,
+  MatProgressBarModule,
   MatSnackBarModule,
   MatToolbarModule
 } from '@angular/material';
@@ -16,6 +17,7 @@ import {
     MatIconModule,
     MatListModule,
     MatMenuModule,
+    MatProgressBarModule,
     MatSnackBarModule,
     MatToolbarModule
   ],
@@ -25,6 +27,7 @@ import {
     MatIconModule,
     MatListModule,
     MatMenuModule,
+    MatProgressBarModule,
     MatSnackBarModule,
     MatToolbarModule
   ]
