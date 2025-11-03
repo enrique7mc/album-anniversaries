1f14f80fd7ad1864768db53d4a1342d307613b5d
enrique7mc <xemc@pm.me>
2025-11-02 21:56:59 -0500

    Update spotify scopes to requests less user data

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -5,7 +5,7 @@ const client_id = 'ed38e1b39051422a94911e7b93e2cd83';
 // Note: Using 127.0.0.1 instead of localhost per Spotify's OAuth requirements
 const redirect_uri_dev = 'http://127.0.0.1:4200';
 const redirect_uri = 'https://album-anniversaries.firebaseapp.com/';
-const scope = 'user-read-private user-read-email user-top-read user-modify-playback-state';
+const scope = 'user-top-read user-modify-playback-state';
 const albumsLocalUrl = 'assets/albums.json';
 const artistsLocalUrl = 'assets/artists.json';
 
